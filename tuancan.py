99c7d45c56ea9fc9d25a88162bd979cc1d936daa
zhanghongru2012 <zhanghongru2012@163.com>
2019-12-19 17:17:23 +0800

    locust_eazy_cpmmit

---
+++ tuancan.py
@@ -0,0 +1,22 @@
+from locust import HttpLocust, TaskSet, task, between
+
+
+class WebsiteTasks(TaskSet):
+    def on_start(self):
+        self.client.post("/login", {
+            "username": "test_user",
+            "password": ""
+        })
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def about(self):
+        self.client.get("/about/")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    wait_time = between(5, 15)
